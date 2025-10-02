4ef21d8d0effe77b939a19eca8254fc633bab1b7
Richmond manalo <richmondmanalo731@gmail.com>
2025-10-02 11:52:57 +0800

    Right but slight wrong
    Right but slight wrong

--- login.js
+++ login.js
@@ -1,8 +1,8 @@
-function signin(){
+function signup(){
     let email = document.getElementById("email").value;
 let pass = document.getElementById("password").value;
 
-    console.log("You enter the enail: " + email);
+    console.log("You enter the email: " + email);
     console.log("You enter the password: " + email);
 
     if (email==="richmondmanalo731@gmail.com"){
@@ -11,10 +11,17 @@ let pass = document.getElementById("password").value;
     console.log("Your email is incorrect.")
     document.getElementById("error-msg").innerHTML = "Your email is correct.";
 }
- if (email==="richmondmanalo731@gmail.com"){
-        console.log("Your email is correct");
+ if (password==="Admin_123"){
+        console.log("Your password is correct");
     }else{
-    console.log("Your email is incorrect.")
-    document.getElementById("error-msg").innerHTML = "Your email is correct.";
+    console.log("Your password is incorrect.")
+    document.getElementById("error-msg").innerHTML = "Your password is correct.";
  }
+ if (login)
+    console.log("Your password is correct." + password);
+
+
+
+
+
 }
