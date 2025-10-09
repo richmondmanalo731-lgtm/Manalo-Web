e55c34d5b41b057722ffea3135ba90c50530430f
Richmond <richmondmanalo731@gmail.com>
2025-10-09 11:52:10 +0800

    handsonexam
    not finish yet

---
+++ handson1D.js
@@ -0,0 +1,29 @@
+function checkmarks(){
+    let name =  document.getElementById("name").value;
+    let grade = document.getElementById("grade").value;
+
+    console.log("You enter the name: "+ name);
+    console.log("You enter your grade: "+ grade);
+
+    if (grade>=95 && grade <=100){
+        console.log("PASSED");
+
+
+
+    } else if (grade>=85 && grade <=94);{
+        console.log("PASSED");
+
+
+
+    } if (grade>=75 && grade <=84){
+    console.log("PASSED");
+
+
+
+    } else if (garde>=0 && grade <=75);
+    console.log("FAILED")
+
+
+    
+    
+}
